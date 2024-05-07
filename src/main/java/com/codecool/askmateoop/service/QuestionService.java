6e22dc9fe4741c90d3cd7f40949c480e84000904
Arva Martin <144056979+arvamartin@users.noreply.github.com>
2024-05-07 10:36:21 +0200

    setup database, DatabaseConn class, getAll method

--- src/main/java/com/codecool/askmateoop/service/QuestionService.java
+++ src/main/java/com/codecool/askmateoop/service/QuestionService.java
@@ -7,6 +7,7 @@ import com.codecool.askmateoop.dao.model.Question;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.sql.SQLException;
 import java.time.LocalDateTime;
 import java.util.List;
 
@@ -20,7 +21,7 @@ public class QuestionService {
         this.questionsDAO = questionsDAO;
     }
 
-    public List<QuestionDTO> getAllQuestions() {
+    public List<QuestionDTO> getAllQuestions() throws SQLException {
         List<Question> allQuestions = questionsDAO.getAllQuestions();
         // TODO convert data to QuestionDTO
         return List.of(new QuestionDTO(1, "Example Title", "Example Description", LocalDateTime.now()));
